c11a8f98c0c74856f9c4ce9c85e1ee600ed586dc
Roman00 <rvasin00@gmail.com>
2020-03-26 19:33:39 +0600

    feat: Добавили файл js и создали переменные

---
+++ js/main.js
@@ -0,0 +1,15 @@
+//Создал переменные
+let money;
+let income;
+let addExpenses;
+let deposit;
+let mission;
+let period;
+
+//Вывел текст в модальное окно с помощью команды 'alert'
+alert('Мое первое задание по Js');
+
+// Вывел текст в консоль разработчика с помощью команды 'console.log'
+console.log('Мое первое задание по Js');
+
+
