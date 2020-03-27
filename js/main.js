437b0e0083128a763d31819f15aa2ee1a869010a
Roman00 <rvasin00@gmail.com>
2020-03-27 13:02:14 +0600

    домашнее задание урока 2

--- js/main.js
+++ js/main.js
@@ -1,15 +1,31 @@
-//Создал переменные
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+const money = 225500;
+  console.log(typeof money);
 
-//Вывел текст в модальное окно с помощью команды 'alert'
-alert('Мое первое задание по Js');
+const income = 'Создание сайтов';
+  console.log(typeof income);
 
-// Вывел текст в консоль разработчика с помощью команды 'console.log'
-console.log('Мое первое задание по Js');
+const addExpenses = 'Налоги, Хостинг, Avacode, Интернет';
+  console.log(addExpenses.length);
+  console.log(addExpenses.toLowerCase());
+  console.log(addExpenses.split(', '));
+
+const deposit = true;
+  console.log(typeof deposit);
+
+const mission = 5000000;
+
+const period = 5;
+  console.log('"Период равен ' + period + ' месяцев" ' + 'и ' + '"Цель заработать ' + mission + ' тенге"');
+
+
+// Создали переменную monthlyIncome 'Бюджет за месяц'
+const monthlyIncome = 225500;
+
+// С помощью математических действий вычислили daileBudget 'Дневной бюджет'
+const daileBudget = monthlyIncome / 30;
+
+// Создали переменную  budgetDay и присвоили пкременную  daileBudget 'Дневной бюджет'
+const budgetDay = daileBudget;
+console.log(budgetDay);
 
 
