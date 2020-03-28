d1c6799f506f3f96d87ca714c976ffba0c42734c
Roman00 <rvasin00@gmail.com>
2020-03-28 08:55:45 +0600

    поправили код

--- js/main.js
+++ js/main.js
@@ -1,31 +1,23 @@
 const money = 225500;
-  console.log(typeof money);
-
 const income = 'Создание сайтов';
-  console.log(typeof income);
-
 const addExpenses = 'Налоги, Хостинг, Avacode, Интернет';
-  console.log(addExpenses.length);
-  console.log(addExpenses.toLowerCase());
-  console.log(addExpenses.split(', '));
-
 const deposit = true;
-  console.log(typeof deposit);
-
 const mission = 5000000;
-
 const period = 5;
-  console.log('"Период равен ' + period + ' месяцев" ' + 'и ' + '"Цель заработать ' + mission + ' тенге"');
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(addExpenses.length);
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(', '));
+console.log(typeof deposit);
+console.log('"Период равен ' + period + ' месяцев" ' + 'и ' + '"Цель заработать ' + mission + ' тенге"');
 
 
-// Создали переменную monthlyIncome 'Бюджет за месяц'
-const monthlyIncome = 225500;
-
-// С помощью математических действий вычислили daileBudget 'Дневной бюджет'
-const daileBudget = monthlyIncome / 30;
-
-// Создали переменную  budgetDay и присвоили пкременную  daileBudget 'Дневной бюджет'
+// Вычисление
+const daileBudget = money / 30;
 const budgetDay = daileBudget;
+
 console.log(budgetDay);
 
 
