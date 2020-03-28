f8df036a1817799d31d2b822d1288cb84776a3dc
Roman00 <rvasin00@gmail.com>
2020-03-28 08:49:38 +0600

    fixed: исправили и поправили код

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
-const num = 266219;
+const money = 225500;
+const income = 'Создание сайтов';
+const addExpenses = 'Налоги, Хостинг, Avacode, Интернет';
+const deposit = true;
+const mission = 5000000;
+const period = 5;
 
-const arr = num.toString().split('').map(n=> Number(n));
+console.log(typeof money);
+console.log(typeof income);
+console.log(addExpenses.length);
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(', '));
+console.log(typeof deposit);
+console.log('"Период равен ' + period + ' месяцев" ' + 'и ' + '"Цель заработать ' + mission + ' тенге"');
 
-const result = arr.reduce((start, end) => {
-  return start * end;
-});
 
-console.log(result);
-
-// возводим полученное число в 3 степень
-console.log(result ** 3);
-
-// выводим первые 2 числа 
-const res = 2176782336;
-
-const sum = res.toString().split('');
-
-console.log(sum[0]);
-console.log(sum[1]);
+// Вычисление budgetDay
+const daileBudget = money / 30;
+const budgetDay = daileBudget;
+console.log(budgetDay);
