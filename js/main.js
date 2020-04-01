85c3c92ddd27117252151672e0f7ae256aaecdd4
Roman00 <rvasin00@gmail.com>
2020-04-01 08:10:23 +0600

    feat: rewrote the code and added functions

--- js/main.js
+++ js/main.js
@@ -1,41 +1,65 @@
-'use strict'
+'use strict';
+const money = prompt('Ваш месячный доход?', '');
+const income = 'Создание сайтов';
+const addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', '');
+const deposit = confirm('Есть ли у вас депозит в банке?');
+const expenses1 = prompt('Введите обязательную статью расходов', '');
+const amount1 = +prompt('Во сколько это обойдется?', '');
+const expenses2 = prompt('Введите обязательную статью расходов', '');
+const amount2 = +prompt('Во сколько это обойдется?', '');
+const mission = 1000000;
+const period = 12;
 
-const lang = 'ru';
-
-const daysWeekRu = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
-const daysWeekEn = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-
-
-// вывод дней недели через условия if & else
-if (lang === 'ru') {
-  console.log(daysWeekRu);
-} else if (lang === 'en') {
-  console.log(daysWeekEn);
+const showTypeOf = function (data) {
+  console.log(typeof(data));
 }
 
-// вывод дней недели через switch case
-switch (lang) {
-  case 'ru':
-    console.log(daysWeekRu);
-    break;
-  case 'en':
-    console.log(daysWeekEn);
-    break;
-}
+showTypeOf(money);
+showTypeOf(income);
+showTypeOf(deposit);
 
-// вывод дней недли через многомерный массив
+console.log(income.length);
 
-const langs = ['ru', 'en'];
-const langsId = langs.indexOf(lang);
-const arrDaysWeek = [
-  daysWeekRu,
-  daysWeekEn
-];
+console.info('Период равен ' + period + ' месяцев');
+console.info('Цель накопить ' + mission + ' KZT');
 
-console.log(arrDaysWeek[langsId]);
+console.log(addExpenses.toLowerCase().split(', '));
+
+const getExpensesMonth = function() {
+  return amount1 + amount2;
+};
+
+console.log('Расходы за месяц ' + getExpensesMonth() + ' KZT');
+
+const getAccumulatedMonth = function() {
+  return money - getExpensesMonth();
+};
+console.log('Бюджет на месяц ' + getAccumulatedMonth() + ' KZT');
+
+const accumulatedMonth = getAccumulatedMonth();
+
+const getTargetMonth = function() {
+  return Math.ceil(mission / accumulatedMonth);
+};
+
+console.log('Цель будет достигнута за ' + getTargetMonth() + ' месяца');
 
 
-const namePerson = 'Евгений',
-      positionPerson = namePerson == 'Артем' ? 'директор': namePerson == 'Максим' ? 'преподаватель' : 'студент';
+const budgetDay = accumulatedMonth / 30;
 
-console.log(positionPerson);
+console.log('Бюджет на день: ' + Math.floor(budgetDay) + ' KZT');
+
+
+const getStatusIncome = function () {
+  if(budgetDay >= 1200) {
+    return ('У вас высокий уровень дохода');
+  } else if (budgetDay >= 600) {
+    return ('У вас средний уровень дохода');
+  } else if (budgetDay >= 0) {
+    return ('К сожалению у вас уровень дохода ниже среднего');
+  } else {
+    return ('Что-то пошло не так');
+  }
+};
+
+console.log(getStatusIncome());
