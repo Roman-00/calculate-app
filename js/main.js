115a688ad9f20e2f2447d551158813b225d81328
Roman00 <rvasin00@gmail.com>
2020-04-05 22:08:14 +0600

    feat: created a function and passed one argument

--- js/main.js
+++ js/main.js
@@ -1,88 +1,19 @@
 'use strict';
-const isNumber = function(n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
-}
 
-let money,
-    start = function() {
-      do {
-        money = prompt('Ваш месячный доход?');
-      }
-      while(!isNumber(money));
-    }
-
-  start();
-
-const appData = {
-  budget: money,
-  budgetDay: 0,
-  budgetMonth: 0,
-  expensesMonth: 0,
-  income: {},
-  addIncome: [],
-  question: [],
-  expenses: {},
-  addExpenses: [],
-  deposit: false,
-  mission: 1000000,
-  period: 12,
-  asking: function() {
-    const addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'кино, интернет, театр');
-          appData.addExpenses = addExpenses.toLowerCase().split(', ');
-          appData.deposit = confirm('Есть ли у вас депозит в банке?');
-          for (let i = 0; i < 2; i++) {
-            let question = prompt("Введите обязательную статью расходов"),
-                answer = prompt("Во сколько это обойдется?");
-            while(!isNumber(answer)) {
-                answer = prompt("Во сколько это обойдется?");
-            }
-            appData.expenses[question] = +answer;
-        }
-  },
-  getExpensesMonth: function() {
-    for (let key in appData.expenses) {
-      appData.expensesMonth += +appData.expenses[key];
-    }
-  },
-  getBudget: function(){
-    appData.budgetMonth = appData.budget - appData.expensesMonth;
-    appData.budgetDay = Math.floor(appData.budgetMonth / 30);
-  },
-  getTargetMonth: function(){
-    return appData.mission / appData.budgetMonth;
-  },
-  getStatusIncome: function () {
-    if(appData.budgetDay >= 1200) {
-      return ('У вас высокий уровень дохода');
-    } else if (appData.budgetDay >= 600) {
-      return ('У вас средний уровень дохода');
-    } else if (appData.budgetDay >= 0) {
-      return ('К сожалению у вас уровень дохода ниже среднего');
-    } else {
-      return ('Что-то пошло не так');
-    }
-  },
-  calcSavedMoney: function () {
-    return appData.budgetMonth * appData.period;
+const newFunction = function (argument) {
+  if(typeof argument !== 'string') {
+    console.log('Функция передает не строку!!');
   }
-};
 
+  // Условия убирают пробелы с обоих сторон и заменяются на ...
+  let extension = argument.trim();
+  if(extension.length > 30) {
+    extension = extension.substring(0, 30) + '...';
+  }
 
-appData.asking();
-appData.getExpensesMonth();
-appData.getBudget();  
-
-console.log('Расходы за месяц: ' + appData.expensesMonth + ' KZT');
-if (appData.getTargetMonth() > 0) {
-console.log("Цель будет достигнута за " + Math.ceil(appData.getTargetMonth()) + ' месяцев');
-} else {
-  console.log('Цель не будет достигнута!');
+  return extension;
 }
 
-console.log(appData.getStatusIncome());
+//newFunction(11);
 
-/*for (let key in appData) {
-  console.log('Наша программа включает в себя данные: ' + key + ' - ' + appData[key]);
-}*/
-
-/*console.log("expenses", appData.expenses);*/
+console.log(newFunction('12345678901234567890asdqwezxcr 12345678901234567890'));
