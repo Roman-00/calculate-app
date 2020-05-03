bcce83f8c6e7a988c6c9f9b619d13512b2f372bb
Roman-00 <58338625+Roman-00@users.noreply.github.com>
2020-05-03 13:54:21 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -11,27 +11,30 @@ const DomElement = function (selector, height, width, bg, fontSize, position) {
   this.position = position;
 };
 
-function keyDownHandler (event) {
-  const div = document.querySelectorAll('block');
-  let left = parseInt(div.marginLeft);
-  let top = parseInt(div.marginTop);
-  let right = parseInt(div.marginRight);
-  let bottom = parseInt(div.margiBottom);
-  
-  if(event.keyCode === 39) {
-    right += -10;
-    div.style.right = right + 'px';
-  } else if(event.keyCode === 38) {
+function keyDownHandler(event)  {
+  const div = document.querySelector('.block');
+  let left = parseInt(div.style.left);
+  let top = parseInt(div.style.top);
+  if (!isNaN(div.style.left)) {
+    left = 0;
+  } 
+  if (!isNaN(div.style.top)) {
+    top = 0;
+  } 
+  if (event.keyCode === 37) {
+    left += -10;
+    div.style.left = left + 'px';  
+  } else if (event.keyCode === 38) {
     top += -10;
     div.style.top = top + 'px';
-  } else if(event.keyCode === 37) {
-    left += -10;
+  } else if (event.keyCode === 39) {
+    left += +10;
     div.style.left = left + 'px';
-  } else if(event.keyCode === 40) {
-    bottom += -10;
-    div.style.bottom = bottom + 'px';
+  } else if (event.keyCode === 40) {
+    top += 10;
+    div.style.top = top + 'px';
   }
-};
+}
 
 DomElement.prototype.result = function () {
   let newElementBlock;
@@ -59,5 +62,3 @@ DomElement.prototype.result = function () {
 
 let newObject = new DomElement('.', '100', '100', 'pink', '12', 'absolute');
 newObject.result();
-
-
